ff87fd7e3c0264c088bf751d81eb7d452b9787a8
sy0223lee <sy0223lee@gmail.com>
2022-06-10 21:11:08 +0900

    <tr>-<Link> 오류나서 다시 복구

--- src/pages/Notice/components/NoticeListComponent.js
+++ src/pages/Notice/components/NoticeListComponent.js
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function NoticeListComponent(props){
+    let Navigate = useNavigate();
+
+    const ToNoticeRead = ()=>{
+        Navigate('/notice/read/'+props.notice_id);
+    };
+
     return (
         <tr className="n_list_row">
             <td className="noticeIndex">{props.notice_id}</td>
-            <Link to={{pathname: '/notice/read/'+props.notice_id}}><td className="noticeTitle">{props.notice_title}</td></Link>
+            <td className="noticeTitle" onClick={ToNoticeRead}>{props.notice_title}</td>
             <td className="noticeDate">{props.notice_date}</td>
             <td className="managerId">{props.manager_id}</td>
         </tr>
